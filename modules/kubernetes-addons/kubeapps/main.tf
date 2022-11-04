9f3a525285550be2ed8ed73462c8255fb06aebb9
Matthew Riedel <matthew.riedel@fluxfederation.com>
2022-11-04 15:24:31 +1300

    Updating KubeApps

--- modules/kubernetes-addons/kubeapps/main.tf
+++ modules/kubernetes-addons/kubeapps/main.tf
@@ -44,6 +44,7 @@ resource "kubernetes_cluster_role_binding_v1" "kubeapps" {
   subject {
     kind = "ServiceAccount"
     name = kubernetes_service_account_v1.kubeapps.metadata[0].name
+    namespace = module.helm_addon.helm_release[0].namespace
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
